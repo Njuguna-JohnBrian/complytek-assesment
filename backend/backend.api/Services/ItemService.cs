0e36803b87898d554689b81a8e6dca48e9a219ac
John Brian <njugunajb96@gmail.com>
2024-02-21 06:41:24 +0300

    create item service

---
+++ backend/backend.api/Services/ItemService.cs
@@ -0,0 +1,21 @@
+﻿using backend.api.Database;
+using backend.api.Database.Entities;
+using backend.api.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.api.Services;
+
+public class ItemService : IItemService
+{
+    private readonly DatabaseContext _context;
+
+    public ItemService(DatabaseContext databaseContext)
+    {
+        _context = databaseContext;
+    }
+
+    public async Task<List<ItemEntity>> GetItems()
+    {
+        return await _context.ItemEntities.ToListAsync();
+    }
+}
