ca678c474c292cd8c87915bafe814d97d45b1bf5
John Brian <njugunajb96@gmail.com>
2024-02-21 14:01:43 +0300

    add migrations

---
+++ backend/backend.api/Migrations/20240221094140_mark iscomplete as false by default.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class markiscompleteasfalsebydefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsComplete",
+                table: "Items",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsComplete",
+                table: "Items",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "INTEGER",
+                oldNullable: true);
+        }
+    }
+}
