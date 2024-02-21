24d209d40b6e2937296be2f330acfdf7946db61c
John Brian <njugunajb96@gmail.com>
2024-02-21 06:41:35 +0300

    create item entity

--- backend/backend.api/Database/Entities/ItemEntity.cs
+++ backend/backend.api/Database/Entities/ItemEntity.cs
@@ -1,8 +1,14 @@
-﻿namespace backend.api.Database.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.api.Database.Entities;
 
 public sealed class ItemEntity
 {
-    
-    public long Id { get; set; }
-    public Guid ItemId { get; set; }
+    [Key] public long Id { get; init; }
+    [Required] public Guid ItemId { get; init; }
+    [Required] public string ItemName { get; init; }
+    public string? ItemDescription { get; init; } = string.Empty;
+    public bool IsComplete { get; init; }
+   public DateTime? CreatedDtm { get; set; } = DateTime.Now;
+    [Timestamp] public byte[] RowVersion { get; set; }
 }
